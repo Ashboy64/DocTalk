09ddfe5095ea85426e2d0235fdecc49a8c38950a
Ashish <ashish.arartc@gmail.com>
2019-01-12 12:39:50 -0800

    Firebase hub file

---
+++ app/src/main/java/com/example/rao_a/doctalk/FirebaseHub.java
@@ -0,0 +1,18 @@
+package com.example.rao_a.doctalk;
+
+public class FirebaseHub {
+
+    public static void createDoctor(){
+
+    }
+
+    public static void createPatient(){
+
+    }
+
+    public static void getSchedue(){
+
+    }
+
+}
+
