a8d5a864dbd293e028cdde583469173b236133c1
Ashish <ashish.arartc@gmail.com>
2019-01-12 18:06:13 -0800

    notifs

--- app/src/main/java/com/example/rao_a/doctalk/MainActivity.java
+++ app/src/main/java/com/example/rao_a/doctalk/MainActivity.java
@@ -1,9 +1,5 @@
 package com.example.rao_a.doctalk;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.os.Build;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
